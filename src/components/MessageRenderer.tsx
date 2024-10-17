864e7f505a782928cd09ae7d2cc48231d97ce0c4
Omar Ashraf <oashraf304@gmail.com>
2024-10-17 18:46:18 +0300

    implement filepond

--- src/components/MessageRenderer.tsx
+++ src/components/MessageRenderer.tsx
@@ -1,7 +1,5 @@
-import { CloudDownload } from "@mui/icons-material";
-import { Modal } from "@mui/material";
+import { CloudDownloadOutlined } from "@mui/icons-material";
 import { motion } from "framer-motion";
-import { useState } from "react";
 import { Message } from "../types/messageTypes";
 import DataTable from "./DataTable";
 
@@ -11,14 +9,6 @@ interface MessageProps {
 
 const MessageRenderer: React.FC<MessageProps> = ({ message }) => {
   const { content, type, sender } = message;
-  const [open, setOpen] = useState(false);
-  const [imageSrc, setImageSrc] = useState("");
-
-  const handleImageClick = (src: string) => {
-    setImageSrc(src);
-    setOpen(true);
-  };
-
   const animationProps = {
     initial: { opacity: 0, y: -10 },
     animate: { opacity: 1, y: 0 },
@@ -26,54 +16,44 @@ const MessageRenderer: React.FC<MessageProps> = ({ message }) => {
     transition: { duration: 0.3 },
   };
 
-  const senderAvatar =
-    sender === "You"
-      ? "https://randomuser.me/api/portraits/men/99.jpg"
-      : "https://randomuser.me/api/portraits/men/10.jpg";
-
   return (
-    <motion.div {...animationProps} className="mb-4 flex items-start space-x-4">
-      <img src={senderAvatar} alt={sender} className="h-8 w-8 rounded-full" />
+    <motion.div {...animationProps} className="mb-4 flex items-start space-x-3">
+      <img
+        src={"https://randomuser.me/api/portraits/men/1.jpg"}
+        alt={sender}
+        className="h-10 w-10 rounded-full"
+      />
       <div>
         <span className="text-[#868686] text-sm">{sender}</span>
-        {type === "text" && <p>{content}</p>}
-        {type === "image" && (
-          <>
+        <div>
+          {type === "text" && <p className="mt-1 text-gray-800">{content}</p>}
+          {type === "image" && (
             <img
-              className="max-w-xs cursor-pointer"
               src={content}
-              alt="message"
-              onClick={() => handleImageClick(content)}
+              alt="Sent image"
+              className="mt-1 h-40 w-fit object-cover rounded-lg shadow-lg cursor-pointer"
+              onClick={() => window.open(content, "_blank")}
             />
-            <Modal open={open} onClose={() => setOpen(false)}>
-              <div className="relative flex items-center justify-center h-screen">
-                <button
-                  onClick={() => setOpen(false)}
-                  className="absolute top-4 right-4 w-7 h-7 flex items-center justify-center bg-white p-2 rounded-full shadow-lg cursor-pointer"
-                >
-                  X
-                </button>
-                <img
-                  src={imageSrc!}
-                  alt="Preview"
-                  className="max-w-full max-h-full object-cover"
-                />
-              </div>
-            </Modal>
-          </>
-        )}
-        {type === "file" && (
-          <div>
-            <a
-              href={content}
-              download
-              className="flex gap-2 items-center rounded-md w-fit p-3 shadow-md text-blue-600 font-medium"
-            >
-              <CloudDownload /> Download File
-            </a>
-          </div>
-        )}
-        {type === "table" && <DataTable data={JSON.parse(content)} />}
+          )}
+          {type === "file" && (
+            <div>
+              <a
+                href={content}
+                download
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex gap-2 items-center rounded-md w-fit p-3 shadow-md text-blue-600 font-medium"
+              >
+                <CloudDownloadOutlined /> Download File
+              </a>
+            </div>
+          )}
+          {type === "table" && (
+            <div className="mt-2">
+              <DataTable data={JSON.parse(content)} />
+            </div>
+          )}
+        </div>
       </div>
     </motion.div>
   );
--- src/components/SendMessageForm.tsx
+++ src/components/SendMessageForm.tsx
@@ -1,137 +1,99 @@
 import { useState } from "react";
-import { Modal } from "@mui/material";
 import { Message } from "../types/messageTypes";
-import {
-  AttachFile,
-  Close,
-  Delete,
-  Send,
-  Description,
-} from "@mui/icons-material";
+import { AttachFile, Send } from "@mui/icons-material";
+import { FilePond, registerPlugin } from "react-filepond";
+import "filepond/dist/filepond.min.css";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import { FilePondFile } from "filepond";
+
+registerPlugin(FilePondPluginImagePreview);
 
 interface SendMessageFormProps {
   sendMessage: (message: Message) => void;
 }
 
 const SendMessageForm: React.FC<SendMessageFormProps> = ({ sendMessage }) => {
-  const [input, setInput] = useState("");
-  const [file, setFile] = useState<File | null>(null);
-  const [preview, setPreview] = useState<string | null>(null);
-  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState<string>("");
+  const [files, setFiles] = useState<Blob[]>([]);
+  const [pondInstance, setPondInstance] = useState<FilePond | null>(null);
 
   const handleSendMessage = () => {
-    if (input || file) {
+    if (input || files.length > 0) {
       const newMessage: Message = {
         id: Date.now(),
-        content: input || URL.createObjectURL(file!),
-        type: file
-          ? file.type.startsWith("image/")
-            ? "image"
-            : "file"
-          : "text",
+        content: input || URL.createObjectURL(files[0]),
+        type:
+          files.length > 0
+            ? files[0].type.startsWith("image/")
+              ? "image"
+              : "file"
+            : "text",
         sender: "You",
         timestamp: new Date(),
       };
 
       sendMessage(newMessage);
       setInput("");
-      setFile(null);
-      setPreview(null);
+      setFiles([]);
+      pondInstance?.removeFiles();
     }
   };
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = e.target.files?.[0];
-    if (selectedFile && selectedFile.type.startsWith("image/")) {
-      setPreview(URL.createObjectURL(selectedFile));
-    } else if (selectedFile) {
-      setPreview(selectedFile.name);
+  const handleFileUpload = (filePondFiles: FilePondFile[]) => {
+    const file = filePondFiles[0]?.file;
+    if (file) {
+      setFiles([file]);
     }
-    setFile(selectedFile || null);
   };
 
-  const handleRemoveFile = () => {
-    setFile(null);
-    setPreview(null);
-    setOpen(false);
+  const handleFileRemove = () => {
+    setFiles([]);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center pt-4 space-y-4">
-      {preview && (
-        <div className="relative flex items-center justify-center w-fit">
-          {file?.type.startsWith("image/") ? (
-            <img
-              src={preview}
-              alt="Preview"
-              className="h-32 w-32 object-cover cursor-pointer rounded-lg shadow-lg"
-              onClick={() => setOpen(true)}
-            />
-          ) : (
-            <div className="flex items-center space-x-2 p-3 bg-gray-100 rounded-lg shadow-md">
-              <Description style={{ fontSize: 30 }} />
-              <span className="text-gray-700 font-medium">{preview}</span>
-            </div>
-          )}
-          <button
-            className="absolute top-0 -right-8 w-6 h-6 bg-red-500 text-white rounded-full flex items-center justify-center shadow-md hover:bg-red-600"
-            onClick={handleRemoveFile}
-          >
-            <Delete style={{ width: "16px", height: "16px" }} />
-          </button>
-        </div>
-      )}
+    <div className="w-full p-4">
+      <div className="flex items-center space-x-3 w-full max-w-full bg-white ">
+        <label
+          htmlFor="file-upload"
+          className="p-2 rounded-full bg-blue-500 hover:bg-blue-600 cursor-pointer text-white"
+          aria-label="Attach file"
+          onClick={() => pondInstance?.browse()}
+        >
+          <AttachFile />
+        </label>
 
-      <div className="p-4 flex items-center space-x-3 w-full max-w-full">
+        <div className="w-full h-fit">
+          <FilePond
+            ref={(pond) => setPondInstance(pond)}
+            allowMultiple={false}
+            onupdatefiles={handleFileUpload}
+            onremovefile={handleFileRemove}
+            acceptedFileTypes={["image/*", "application/json"]}
+            labelIdle='Drag & Drop your file or <span class="filepond--label-action">Browse</span>'
+            className="custom-filepond"
+          />
+        </div>
+      </div>
+
+      <div className="flex items-center space-x-3 w-full max-w-full bg-white">
         <input
           type="text"
           className="flex-grow p-2 border rounded-md shadow-sm focus:ring-2 focus:ring-blue-400 outline-none"
-          placeholder="Type your message..."
+          placeholder="Type message here"
           aria-label="Message input"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
 
-        <input
-          type="file"
-          className="hidden"
-          id="file-upload"
-          onChange={handleFileChange}
-        />
-        <label
-          htmlFor="file-upload"
-          className="py-2 px-4 bg-blue-500 text-white rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition duration-150 ease-in-out"
-          aria-label="Upload file"
-        >
-          <AttachFile />
-        </label>
-
         <button
-          className="py-2 px-4 bg-green-500 text-white rounded-md shadow-md hover:bg-green-600 transition duration-150 ease-in-out"
+          className="p-2 bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition duration-150 ease-in-out"
           onClick={handleSendMessage}
           aria-label="Send message"
         >
           <Send />
         </button>
       </div>
-
-      <Modal open={open} onClose={() => setOpen(false)}>
-        <div className="relative flex items-center justify-center h-screen bg-black bg-opacity-70">
-          <button
-            onClick={() => setOpen(false)}
-            className="absolute top-4 right-4 w-8 h-8 bg-white p-2 rounded-full shadow-lg cursor-pointer hover:bg-gray-200 flex items-center justify-center"
-          >
-            <Close />
-          </button>
-          {file?.type.startsWith("image/") && (
-            <img
-              src={preview!}
-              alt="Preview"
-              className="max-w-full max-h-full object-cover rounded-lg"
-            />
-          )}
-        </div>
-      </Modal>
     </div>
   );
 };
